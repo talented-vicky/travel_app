3e93b79541b5b97ebf47117dde47f6af5156e334
talented_vicky <victorotubure7@gmail.com>
2024-04-25 12:28:49 +0100

    created layout for all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const initItems = [
+  {
+    id: 1, 
+    desc: "passports", 
+    qty: 2, 
+    packed: false
+  },
+  {
+    id: 2, 
+    desc: "socks", 
+    qty: 12, 
+    packed: false
+  },
+]
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header></Header>
+      <Form></Form>
+      <PackList></PackList>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+function Header() {
+  return <div>🌴 Far away 👜 </div>
+}
+function Form() {
+  return (
+    <div className="form">
+      <h3> what're you taking for your trip? </h3>
+    </div>
+  )
+}
+function PackList() {
+  return (
+    <div className="list">
+      <ul>
+        {initItems.map(data => 
+          <Item item={data}/>
+        )}
+      </ul>
+    </div>
+  )
+}
+function Item({item}) {
+  return <li> {item.desc} <span> -- {item.qty} </span></li>
+}
+function Footer() {
+  return (
+    <footer className="stats">
+      <em> You have 13 items from list </em>
+    </footer>
+  )
+}
